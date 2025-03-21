7e5d702a4e0ffcc8ef8d8433ee5b7280a21a050b
joontaek-lim <topingfm@gmail.com>
2025-03-22 08:49:47 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import SeoGeneratorApp from "@/components/ui/SeoGeneratorApp";
+import SeoGeneratorApp from "../components/ui/SeoGeneratorApp";
 
 export default function Home() {
   return <SeoGeneratorApp />;
